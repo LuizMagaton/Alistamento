c6325f3e25ecbc6eac6295c259bcab6ec0cb61b1
LuizMagaton <luizmagaton22@gmail.com>
2025-08-24 23:56:40 -0300

    Bug Fix
    Codigo arrumado e pronto

--- alistamento.py
+++ alistamento.py
@@ -1,22 +1,22 @@
 from datetime import date
-ano_atual = date.today().year
-mes_atual = date.today().month
-dia_atual = date.today().day
 
-print (f"{dia_atual}/{mes_atual}/{ano_atual}")
+# Data atual
+hoje = date.today()
+print(hoje.strftime("%d/%m/%Y"))
 
 ano = int(input("Qual ano voce nasceu?: "))
 mes = int(input("Qual mes voce nasceu?: "))
 dia = int(input("Qual dia voce nasceu?: "))
 
-idade = ano_atual - ano
-diferenca_ano = idade - 18
-diferenca_mes = mes_atual - mes
-diferenca_dia = dia_atual - dia
+nascimento = date(ano, mes, dia)
 
-if idade == 18:
+alistamento = date(ano + 18, mes, dia)
+
+if hoje == alistamento:
     print("A hora é agora, Vá se alistar!")
-elif idade < 18:
-    print (f"Precisara se alistar daqui à {diferenca_ano} anos {diferenca_mes} meses {diferenca_dia} dias")
+elif hoje < alistamento:
+    diferenca = alistamento - hoje
+    print(f"Precisará se alistar daqui a {diferenca.days // 365} anos, {(diferenca.days % 365) // 30} meses e {(diferenca.days % 365) % 30} dias")
 else:
-    print (f"Já se alistou hà {diferenca_ano} ano {diferenca_mes} meses {diferenca_dia} dias")
+    diferenca = hoje - alistamento
+    print(f"Já passou do alistamento há {diferenca.days // 365} anos, {(diferenca.days % 365) // 30} meses e {(diferenca.days % 365) % 30} dias")
